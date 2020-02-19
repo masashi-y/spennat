1f2dfe6f1612a0886f127a820bb82382b1103780
Masashi Yoshikawa <yoshikawa.masashi.yh8@is.naist.jp>
2020-02-19 18:30:38 +0900

    update

--- experiment_utils.py
+++ experiment_utils.py
@@ -2,11 +2,10 @@ import os
 import numpy as np
 
 import tensorflow as tf
-import tensorflow.summary
 
 class TensorBoard:
     def __init__(self, logdir):
-        self.writer = tf.summary.FileWriter(logdir)
+        self.writer = tf.summary.create_file_writer(logdir)
         self.epoch = 0
         self.obj_count = 0
 
@@ -14,10 +13,8 @@ class TensorBoard:
         self.writer.close()
 
     def log_scalar(self, tag, value, global_step):
-        summary = tf.Summary()
-        summary.value.add(tag=tag, simple_value=value)
-        self.writer.add_summary(summary, global_step=global_step)
-        self.writer.flush()
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step=global_step)
 
     def update_epoch(self):
         self.epoch += 1
@@ -25,34 +22,9 @@ class TensorBoard:
     def plot_for_current_epoch(self, tag, value):
         self.log_scalar(tag, value, self.epoch)
 
-    def plot_hist_for_current_epoch(self, tag, histogram):
-        self.log_histogram(histogram, tag, self.epoch)
-
     def plot_obj_val(self, value):
         self.obj_count += 1
         self.log_scalar('Training Objective Value', value, self.obj_count)
-
-    def create_histogram(self, min_val, max_val, num_bins):
-        return np.histogram([], bins=num_bins, range=(min_val, max_val))
-
-    def add_points_to_histogram(self, histogram, points):
-        new_hist = np.histogram(points, bins=histogram[1])
-        return (new_hist[0] + histogram[0], new_hist[1])
-
-    def log_histogram(self, histogram, tag, global_step):
-        tf_hist = tf.HistogramProto()
-        bin_edges = histogram[1][1:]
-        for edge in bin_edges:
-            tf_hist.bucket_limit.append(edge)
-        for count in histogram[0]:
-            tf_hist.bucket.append(count)
-        tf_hist.min = histogram[1][0]
-        tf_hist.max = histogram[1][-1]
-
-        summary = tf.Summary()
-        summary.value.add(tag=tag, histo=tf_hist)
-        self.writer.add_summary(summary, global_step=global_step)
-        self.writer.flush()
 
     def __enter__(self):
         return self
--- spen.py
+++ spen.py
@@ -9,9 +9,10 @@ from omegaconf import DictConfig
 
 import torch
 from torch.utils.data import DataLoader, Dataset
-from torch.autograd import Variable
+from torch.autograd import variable
 from torch.distributions.bernoulli import Bernoulli
 import torch.nn as nn
+import torch.nn.functional as F
 
 from experiment_utils import TensorBoard
 
@@ -29,14 +30,22 @@ def onehot(x):
     return x1.view(x.size(0), -1)
 
 
+def get_learing_conf(model, cfg):
+    return dict(
+        params=[p for p in model.parameters() if p.requires_grad],
+        lr=cfg.learning_rate,
+        weight_decay=cfg.weight_decay,
+        momentum=cfg.momentum
+    )
+
 class FeatureEnergyNetwork(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         in_dim, out_dim = INPUTS, 150
         layers = []
-        for _ in range(cfg.num_layers-1):
-            if cfg.unary_dropout is not None:
-                layers.append(nn.Dropout(cfg.unary_dropout))
+        for _ in range(cfg.num_layers - 1):
+            if cfg.dropout is not None:
+                layers.append(nn.Dropout(cfg.dropout))
             linear = nn.Linear(in_dim, out_dim)
             linear.weight.data.normal_(std=np.sqrt(2. / in_dim))
             linear.bias.data.fill_(0.)
@@ -46,6 +55,7 @@ class FeatureEnergyNetwork(nn.Module):
         layers[-1].weight.data.normal_(std=np.sqrt(2. / in_dim))
         layers[-1].bias.data.fill_(0.)
         self.model = nn.Sequential(*layers)
+        self.learning_conf = get_learing_conf(self, cfg)
 
     def forward(self, xs):
         """
@@ -64,80 +74,59 @@ class FeatureEnergyNetwork(nn.Module):
 class GlobalEnergyNetwork(nn.Module):
     def __init__(self, cfg):
         super().__init__()
-        hidden_size = cfg.t_hidden_size
         self.model = nn.Sequential(
-            nn.Linear(LABELS, hidden_size, bias=False),
+            nn.Linear(LABELS, cfg.hidden_size, bias=False),
             nn.Softplus(),
-            nn.Linear(hidden_size, 1, bias=False))
+            nn.Linear(cfg.hidden_size, 1, bias=False))
+        self.learning_conf = get_learing_conf(self, cfg)
 
-    def forward(self, ys, pots):
-        local_energy = (ys * pots).sum(dim=1)
-        ys = ys.reshape(-1, 2)[:, 1].reshape(ys.size(0), LABELS)
-        label_energy = self.model(ys).squeeze()
-        return local_energy + label_energy
+    def forward(self, ys, potentials):
+        global_energy = (ys * potentials).sum(dim=1)
+        label_energy = self.model(ys.view(-1, LABELS, 2)[:, :, 1]).squeeze()
+        return global_energy + label_energy
         
 
 class UnaryModel(nn.Module):
     def __init__(self, num_nodes, num_vals, cfg):
         super().__init__()
-        self.model = FeatureEnergyNetwork(cfg)
+        self.feature_network = FeatureEnergyNetwork(cfg.feature_network)
         self.num_nodes = num_nodes
         self.num_vals = num_vals
 
-    def get_optimizer(self, cfg):
-        opt = torch.optim.SGD(
-            [p for p in self.parameters() if p.requires_grad],
-            lr=cfg.unary_lr,
-            weight_decay=cfg.get('unary_wd', 0.),
-            momentum=cfg.get('unary_wd', 0.)
-        )
-        return opt
+    def get_optimizer(self):
+        return torch.optim.SGD(**self.feature_network.learning_conf)
 
-    def loss(self, epoch, xs, onehot_ys):
-        pred = self.model(xs)
-        loss = nn.CrossEntropyLoss()
-        return loss(pred.view(-1, self.num_vals), onehot_ys.view(-1))
+    def loss(self, epoch, xs, ys):
+        pred = self.feature_network(xs)
+        return F.cross_entropy(pred.view(-1, self.num_vals), ys.view(-1))
 
-    def calculate_beliefs(self, xs):
-        batch_size = xs.size(0)
-        pred = self.model(xs)
-        return nn.Softmax(dim=1)(pred.view(-1, self.num_vals)).view(batch_size, -1)
+    def predict_beliefs(self, xs):
+        pred = self.feature_network(xs)
+        return F.softmax(pred.view(-1, self.num_nodes, self.num_vals), dim=2)
 
     def predict(self, xs):
-        batch_size = xs.size(0)
-        pred = self.model(xs)
-        return pred.view(-1, self.num_vals).argmax(dim=1).view(batch_size, -1)
+        pred = self.feature_network(xs)
+        return pred.view(-1, self.num_nodes, self.num_vals).argmax(dim=2)
 
 
 class SPENModel(nn.Module):
     def __init__(self, num_nodes, num_vals, cfg):
         super().__init__()
-        self.feature_network = FeatureEnergyNetwork(cfg)
-        self.global_network = GlobalEnergyNetwork(cfg)
+        self.feature_network = FeatureEnergyNetwork(cfg.feature_network)
+        self.global_network = GlobalEnergyNetwork(cfg.global_network)
         self.num_nodes = num_nodes
         self.num_vals = num_vals
-        self.inference_iteration = cfg.num_inf_itrs
-        self.inf_lr = cfg.get('inf_lr', 0.05)
-        self.use_linear_decay = cfg.use_linear_decay
-        self.inf_eps = cfg.get('inf_eps', None)  # 1e-4
-        self.inf_region_eps = cfg.get('inf_region_eps', None)  # None
+        self.inference_iterations = cfg.inference.iterations
+        self.inference_learning_rate = cfg.inference.learning_rate
+        self.inference_eps = cfg.inference.eps
+        self.use_linear_decay = cfg.inference.use_sqrt_decay
         self.entropy_coef = cfg.entropy_coef
 
-    def get_optimizer(self, cfg):
-        unary_conf = dict(
-            params=[p for p in self.feature_network.parameters() if p.requires_grad],
-            lr=cfg.unary_lr,
-            weight_decay=cfg.get('unary_wd', 0.),
-            momentum=cfg.get('unary_mom', 0.)
-        )
-        unary_t_conf = dict(
-            params=[p for p in self.global_network.parameters() if p.requires_grad],
-            lr=cfg.t_unary_lr,
-            weight_decay=cfg.get('t_unary_wd', 0.),
-            momentum=cfg.get('t_unary_mom', 0.)
-        )
-        opt = torch.optim.SGD([unary_conf, unary_t_conf])
-        return opt
+    def get_optimizer(self):
+        return torch.optim.SGD([
+            self.feature_network.learning_conf,
+            self.global_network.learning_conf
+        ])
 
     def _random_probabilities(self, batch_size):
         """returns a tensor with shape (batch size, self.num_nodes * self.num_vals),
@@ -157,30 +146,28 @@ class SPENModel(nn.Module):
                                      .expand(batch_size, -1) \
                                      .contiguous()
 
-    def _emd_inf(self, pots):
-        batch_size = pots.size(0)
-        pots = pots.detach()
-        pots.requires_grad = False
+    def _gradient_based_inference(self, potentials):
+        batch_size = potentials.size(0)
+        potentials = potentials.detach()
         pred = self._random_probabilities(batch_size)
         prev = pred
-        region_prediction_eps = float('inf')
-        for iteration in range(1, self.inference_iteration):
+        for iteration in range(1, self.inference_iterations):
             self.global_network.zero_grad()
-            pred_var = Variable(pred, requires_grad=True)
-            energy = self.global_network(pred_var, pots) \
-                   - self.entropy_coef * (pred_var * torch.log(pred_var + EPS)).sum(dim=1)
+            pred = pred.detach().requires_grad_()
+            energy = self.global_network(pred, potentials) \
+                   - self.entropy_coef * (pred * torch.log(pred + EPS)).sum(dim=1)
             energy.sum().backward()
 
             if self.use_linear_decay:
-                lr = self.inf_lr / iteration
+                lr = self.inference_learning_rate / iteration
             else:
-                lr = self.inf_lr / np.sqrt(iteration)
-            result = lr * pred_var.grad.view(-1, self.num_vals)
-            max_result, _ = torch.max(result, dim=1)
-            result = pred.view(-1, self.num_vals) * torch.exp(result - max_result.view(-1, 1))
-            pred = (result / (result.sum(dim=1, keepdim=True) + EPS)).view(batch_size, -1)
-            region_prediction_eps = torch.norm(prev.view(-1, self.num_vals) - pred.view(-1, self.num_vals), dim=1).max().item()
-            if self.inf_region_eps is not None and region_prediction_eps < self.inf_region_eps:
+                lr = self.inference_learning_rate / np.sqrt(iteration)
+            lr_grad = lr * pred.grad.view(-1, self.num_vals)
+            max_grad, _ = lr_grad.max(dim=1, keepdim=True)
+            pred = pred.view(-1, self.num_vals) * torch.exp(lr_grad - max_grad)
+            pred = (pred / (pred.sum(dim=1, keepdim=True) + EPS)).view(batch_size, -1)
+            eps = (prev - pred).view(-1, self.num_vals).norm(dim=1).max().item()
+            if self.inference_eps is not None and eps < self.inference_eps:
                 break
             prev = pred
         return pred
@@ -188,36 +175,36 @@ class SPENModel(nn.Module):
     def loss(self, epoch, xs, ys):
         self.global_network.train()
         self.feature_network.train()
-        pots = self.feature_network(xs)
-        preds = self._emd_inf(pots)
+        potentials = self.feature_network(xs)
+        preds = self._gradient_based_inference(potentials)
         self.zero_grad()
-        pred_energy = self.global_network(preds, pots)
-        true_energy = self.global_network(ys, pots)
+        pred_energy = self.global_network(preds, potentials)
+        true_energy = self.global_network(ys, potentials)
         loss = pred_energy - true_energy \
              - self.entropy_coef * (preds * torch.log(preds + EPS)).sum(dim=1)
         return loss.mean()
 
-    def calculate_beliefs(self, xs):
-        pots = self.feature_network(xs)
-        preds = self._emd_inf(pots)
+    def predict_beliefs(self, xs):
+        potentials = self.feature_network(xs)
+        preds = self._gradient_based_inference(potentials)
         return preds
 
     def predict(self, xs):
-        pots = self.feature_network(xs)
-        preds = self._emd_inf(pots)
-        preds = preds.view(-1, self.num_vals).argmax(dim=1).view(-1, self.num_nodes)
+        potentials = self.feature_network(xs)
+        preds = self._gradient_based_inference(potentials)
+        preds = preds.view(-1, self.num_nodes, self.num_vals).argmax(dim=2)
         return preds
-            
 
-def load_bibtex(path):
+
+def load_bibtex(path, device):
     xs, ys = [], []
     with open(path) as f:
         for sample in arff.load(f)['data']:
             sample = list(map(int, sample))
             xs.append(sample[:-LABELS])
             ys.append(sample[-LABELS:])
-    xs = torch.tensor(xs, dtype=torch.float32)
-    ys = torch.tensor(ys, dtype=torch.int64)
+    xs = torch.tensor(xs, dtype=torch.float, device=device)
+    ys = torch.tensor(ys, dtype=torch.long, device=device)
     return xs, ys
 
 
@@ -225,7 +212,7 @@ class BibtexDataset(Dataset):
     def __init__(self, xs, ys, flip_prob=None):
         self.xs = xs
         self.ys = ys
-        self.onehot_ys = onehot(self.ys)
+        self.onehot_ys = onehot(ys)
         if flip_prob is not None:
             self.bernoulli = Bernoulli(probs=flip_prob)
         else:
@@ -237,17 +224,20 @@ class BibtexDataset(Dataset):
     def __getitem__(self, indices):
         xs = self.xs[indices, :]
         if self.bernoulli:
-            mask = self.bernoulli.sample(torch.Size([INPUTS]))
+            mask = self.bernoulli.sample((INPUTS,))
             xs = xs * (1 - mask) + (1 - xs) * mask
         return xs, self.ys[indices, :], self.onehot_ys[indices, :]
 
 
-def test(model, dataset, cfg):
+def test(model, dataset, cfg, threshold=None):
     total_f1 = total_precision = total_recall = total_correct = total_vars = 0
-    for xs, ys, _ in DataLoader(
-            dataset, pin_memory=cfg.gpu, batch_size=cfg.batch_size):
-        preds = model.predict(xs)
-        correct = (preds * ys).sum(1).float()
+    for xs, ys, _ in DataLoader(dataset, batch_size=cfg.batch_size):
+        if threshold is None:
+            preds = model.predict(xs)
+        else:
+            node_beliefs = model.predict_beliefs(xs).reshape(-1, 2)[:, 1]
+            preds = (node_beliefs > threshold).long()
+        correct = (preds * ys).sum(1)
         total_correct += (preds == ys).float().sum()
         total_vars += ys.numel()
         prec = correct / (preds.sum(1).float() + EPS)
@@ -269,13 +259,11 @@ def test_with_thresholds(model, dataset, cfg):
     total_precs = np.zeros_like(thresholds)
     total_recs = np.zeros_like(thresholds)
     total_f1s = np.zeros_like(thresholds)
-    for xs, ys, _ in DataLoader(
-            dataset, pin_memory=cfg.gpu, batch_size=cfg.batch_size):
+    for xs, ys, _ in DataLoader(dataset, batch_size=cfg.batch_size):
         num_vars += ys.numel()
-        node_beliefs = model.calculate_beliefs(
-            xs).reshape(-1, 2)[:, 1]
+        node_beliefs = model.predict_beliefs(xs).reshape(-1, 2)[:, 1]
         for i, threshold in enumerate(thresholds):
-            preds = (node_beliefs > threshold).long().view(-1, 159)
+            preds = (node_beliefs > threshold).long().view(-1, LABELS)
             correct = (preds * ys).sum(1).float()
             prec = correct / (preds.sum(1).float() + EPS)
             rec = correct / (ys.sum(1).float() + EPS)
@@ -291,9 +279,15 @@ def test_with_thresholds(model, dataset, cfg):
     return (accs[best], precs[best], recs[best], f1s[best]), thresholds[best]
 
 
-def save_model(model, path, base_name, cfg):
-    pass
+def save_model(model, file_path):
+    with open(file_path, 'wb') as f:
+        torch.save(model.state_dict(), f)
 
+
+def load_model(model, file_path):
+    with open(file_path, 'rb') as f:
+        model.load_state_dict(torch.load(f))
+            
 
 def train(model, train_data, val_data, cfg, train_logger, val_logger):
     global best_f1, best_epoch, best_threshold
@@ -305,7 +299,7 @@ def train(model, train_data, val_data, cfg, train_logger, val_logger):
         if cfg.tune_thresholds:
             test_fun = test_with_thresholds
         else:
-            test_fun = lambda *args: test(*args), None
+            test_fun = lambda *args: (test(*args), None)
 
         (acc, prec, rec, f1), threshold = test_fun(model, train_data, cfg)
         logger.info(
@@ -325,19 +319,18 @@ def train(model, train_data, val_data, cfg, train_logger, val_logger):
             if cfg.tune_thresholds:
                 best_threshold = threshold
             logger.info('new best f1 found, saving model')
-            save_model(model, cfg.working_dir, 'model', cfg)
+            save_model(model, 'best_model')
 
-        save_model(model, cfg.working_dir, 'model_checkpoint', cfg)
+        save_model(model, f'model_checkpoint_{epoch}')
         subset = 'val' if val_data is not None else 'train'
         logger.info(
-            f'best {subset} results (epoch {best_epoch}, thresh {best_threshold}): {best_f1}')
+            f'best {subset} results (epoch {best_epoch}, threshold {best_threshold}): {best_f1}')
 
     train_data_loader = DataLoader(train_data,
                                    batch_size=cfg.batch_size,
-                                   pin_memory=cfg.gpu,
                                    shuffle=True,
                                    drop_last=True)
-    opt = model.get_optimizer(cfg)
+    opt = model.get_optimizer()
     for epoch in range(cfg.num_epochs):
         logger.info(f'epoch {epoch + 1}')
         train_logger.update_epoch()
@@ -355,33 +348,45 @@ def train(model, train_data, val_data, cfg, train_logger, val_logger):
             loss.backward()
             avg_loss += loss
             count += 1
-            if cfg.get('clip_grad', False):
+            if cfg.clip_grad:
                 nn.utils.clip_grad_value_(model.parameters(), cfg.clip_grad)
-            elif cfg.get('clip_grad_norm', False):
+            elif cfg.clip_grad_norm:
                 nn.utils.clip_grad_norm_(
                     model.parameters(), cfg.clip_grad_norm)
             opt.step()
-        train_logger.plot_obj_val(avg_loss / count)
+        train_logger.plot_obj_val((avg_loss / count).item())
     validation(cfg.num_epochs)
 
 
 @hydra.main(config_path='config.yaml')
 def main(cfg: DictConfig) -> None:
-    print(cfg.pretty())
-    print(cfg.get('aaa', False))
-    cwd = Path(hydra.utils.get_original_cwd())
+    logger.info(cfg.pretty())
 
-    train_xs, train_ys = load_bibtex(cwd / cfg.train)
+    if cfg.device >= 0:
+        device = torch.device('gpu', cfg.device)
+    else:
+        device = torch.device('cpu')
+
+    train_xs, train_ys = load_bibtex(
+        hydra.utils.to_absolute_path(cfg.train), device)
     index = int(len(train_xs) * cfg.train_ratio)
-    train_data = BibtexDataset(
-        train_xs[:index, :], train_ys[:index, :], flip_prob=cfg.flip_prob)
-    val_data = BibtexDataset(train_xs[index:, :], train_ys[index:, :])
-    # test_data = BibtexDataset(*load_bibtex(cwd / cfg.test))
+    train_data = BibtexDataset(train_xs[:index, :],
+                               train_ys[:index, :],
+                               flip_prob=cfg.flip_prob)
+    val_data = BibtexDataset(train_xs[index:, :],
+                             train_ys[index:, :])
+                             
+    test_data = BibtexDataset(
+        *load_bibtex(hydra.utils.to_absolute_path(cfg.test), device))
 
-    model = UnaryModel(LABELS, 2, cfg)
+    model = hydra.utils.instantiate(cfg.model, LABELS, 2, cfg).to(device)
+    if cfg.pretrained_unary:
+        unary_model = UnaryModel(LABELS, 2, cfg)
+        load_model(unary_model, cfg.pretrained_unary)
+        model.feature_network = unary_model.feature_network
 
-    with TensorBoard(cwd / cfg.working_dir / 'train') as train_logger, \
-            TensorBoard(cwd / cfg.working_dir / 'val') as val_logger:
+    with TensorBoard(f'{cfg.model.name}_train') as train_logger, \
+            TensorBoard(f'{cfg.model.name}_val') as val_logger:
         train(model,
               train_data,
               val_data,
@@ -389,6 +394,18 @@ def main(cfg: DictConfig) -> None:
               train_logger,
               val_logger)
 
+    logger.info('finding best threshold on validation data...')
+    (val_acc, val_prec ,val_rec ,val_f1), threshold = test_with_thresholds(model, val_data, cfg)
+    logger.info('testing on training data...')
+    train_acc, train_prec ,train_rec ,train_f1 = test(model, train_data, cfg, threshold=threshold)
+    logger.info('testing on test data...')
+    test_acc, test_prec ,test_rec ,test_f1 = test(model, test_data, cfg, threshold=threshold)
+
+    logger.info(f'final results: (threshold: {threshold})')
+    logger.info(f'train: {train_acc}, {train_prec}, {train_rec}, {train_f1}')
+    logger.info(f'val:   {val_acc}, {val_prec}, {val_rec}, {val_f1}')
+    logger.info(f'test:  {test_acc}, {test_prec}, {test_rec}, {test_f1}')
+            
 
 if __name__ == '__main__':
     main()
