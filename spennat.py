fd0ff56bbe97599ce6c55e0fb60155eb334f2f09
Yoshikawa Masashi <yoshikawa.masashi.yh8@is.naist.jp>
2020-03-09 12:05:12 +0900

    update

--- spennat.py
+++ spennat.py
@@ -25,10 +25,17 @@ logger = logging.getLogger(__file__)
 EPS = 1e-6
 
 def get_device(gpu_id):
-    if cfg.device >= 0:
-        return torch.device('cuda', cfg.device)
+    if gpu_id is not None and gpu_id >= 0:
+        return torch.device('cuda', gpu_id)
     else:
         return torch.device('cpu')
+
+
+def onehot(x):
+    x0 = x.view(-1, 1)
+    x1 = x.new_zeros(len(x0), 2, dtype=torch.float)
+    x1.scatter_(1, x0, 1)
+    return x1.view(x.size() + (2,))
 
 
 def get_learing_conf(model, cfg):
@@ -53,7 +60,7 @@ class FeatureNetwork(nn.Module):
             xs {torch.Tensor} -- (source sent length, batch size, hidden size)
         
         Returns:
-            torch.Tensor -- (source sent length, batch size, hidden size)
+            torch.Tensor -- (source sent length, batch size, d_model)
         """
         xs = self.dropout(self.linear(xs))
         return xs
@@ -80,15 +87,15 @@ class AttentionalEnergyNetwork(nn.Module):
     def forward(self, ys, potentials):
         """
         Arguments:
-            ys {torch.Tensor} -- (batch size, target sent length, bit size)
-            potentials {torch.Tensor} -- (batch size, source sent length, hidden size)
+            ys {torch.Tensor} -- (target sent length, batch size, bit size, 2)
+            potentials {torch.Tensor} -- (source sent length, batch size, hidden size)
         
         Returns:
             torch.Tensor -- (batch size,)
         """
-        ys = self.dropout(self.linear(ys))
+        ys = self.dropout(self.linear(ys[:, :, :, 1]))
         ys = self.model(ys, potentials)
-        ys = self.linear_out(ys).squeeze().sum(1)
+        ys = self.linear_out(ys).squeeze().sum(0)
         return ys
 
 
@@ -113,7 +120,7 @@ class SPENModel(nn.Module):
         ])
 
     def _random_probabilities(self, batch_size, target_size, device=None):
-        """returns a tensor with shape (target sent length, batch size, bit size)
+        """returns a tensor with shape (target sent length, batch size, bit size, 2)
         that sums to one at the last dimension
         
         Arguments:
@@ -122,13 +129,11 @@ class SPENModel(nn.Module):
             device {torch.device} -- torch device
         
         Returns:
-            torch.Tensor -- (target sent length, batch size, bit size)
+            torch.Tensor -- (target sent length, batch size, bit size, 2)
         """
-        x = torch.rand(target_size, self.bit_size + 1, device=device)
-        x[:, 0] = 0.
-        x[:, -1] = 1.
-        x, _ = x.sort(1)
-        return (x[:, 1:] - x[:, :-1])[:, None, :].expand(-1, batch_size, -1).contiguous()
+        x = torch.rand(target_size, self.bit_size, 2, device=device)
+        x[:, :, 0] = 1 - x[:, :, 1]
+        return x[:, None, :, :].expand(-1, batch_size, -1, -1).contiguous()
 
     def _gradient_based_inference(self, potentials, max_target_length):
         """
@@ -136,7 +141,7 @@ class SPENModel(nn.Module):
             potentials {torch.Tensor} -- (source sent length, batch size, hidden size)
         
         Returns:
-            torch.Tensor -- (target sent length, batch size, bit size)
+            torch.Tensor -- (target sent length, batch size, bit size, 2)
         """
         batch_size = potentials.size(1)
         potentials = potentials.detach()
@@ -148,7 +153,7 @@ class SPENModel(nn.Module):
             self.global_network.zero_grad()
             pred = pred.detach().requires_grad_()
             energy = self.global_network(pred, potentials) \
-                   - self.entropy_coef * (pred * torch.log(pred + EPS)).sum(dim=1)
+                   - self.entropy_coef * (pred * torch.log(pred + EPS)).sum(dim=(0,2,3))
             eps = torch.abs(energy - prev_energy).max().item()
             if self.inference_eps is not None and eps < self.inference_eps:
                 break
@@ -159,11 +164,11 @@ class SPENModel(nn.Module):
                 lr = self.inference_learning_rate / np.sqrt(iteration)
             else:
                 lr = self.inference_learning_rate / iteration
-            lr_grad = lr * pred.grad.view(-1, self.num_vals)
-            max_grad, _ = lr_grad.max(dim=1, keepdim=True)
-            pred = pred.view(-1, self.num_vals) * torch.exp(lr_grad - max_grad)
-            pred = (pred / (pred.sum(dim=1, keepdim=True) + EPS)).view(batch_size, -1)
-            eps = (prev - pred).view(-1, self.num_vals).norm(dim=1).max().item()
+            lr_grad = lr * pred.grad
+            max_grad, _ = lr_grad.max(dim=-1, keepdim=True)
+            pred = pred * torch.exp(lr_grad - max_grad)
+            pred = pred / (pred.sum(dim=-1, keepdim=True) + EPS)
+            eps = (prev - pred).norm(dim=-1).max().item()
             if self.inference_region_eps is not None and eps < self.inference_region_eps:
                 break
             prev = pred
@@ -173,10 +178,10 @@ class SPENModel(nn.Module):
         """
         Arguments:
             xs {torch.Tensor} -- (source sent length, batch size, hidden size)
-            ys {torch.Tensor} -- (target sent length, batch size, bit size)
+            ys {torch.Tensor} -- (target sent length, batch size, bit size, 2)
         
         Returns:
-            torch.Tensor -- loss
+            torch.Tensor -- loss (batch size,)
         """
         max_target_length = ys.size(0)
         potentials = self.feature_network(xs)
@@ -185,19 +190,33 @@ class SPENModel(nn.Module):
         pred_energy = self.global_network(preds, potentials)
         true_energy = self.global_network(ys, potentials)
         loss = pred_energy - true_energy \
-             - self.entropy_coef * (preds * torch.log(preds + EPS)).sum(dim=1)
+             - self.entropy_coef * (preds * torch.log(preds + EPS)).sum(dim=(0,2,3))
         return loss.mean()
 
     def predict_beliefs(self, xs, max_target_length):
+        """
+        Arguments:
+            xs {torch.Tensor} -- (source sent length, batch size, hidden size)
+            max_target_length {int} -- max target sentence length
+        
+        Returns:
+            torch.Tensor -- (max target sent length, batch size, bit size, 2)
+        """
         potentials = self.feature_network(xs)
-        preds = self._gradient_based_inference(potentials)
+        preds = self._gradient_based_inference(potentials, max_target_length)
         return preds
 
     def predict(self, xs, max_target_length):
-        potentials = self.feature_network(xs, max_target_length)
-        preds = self._gradient_based_inference(potentials, max_target_length)
-        preds = preds.view(-1, self.num_nodes, self.num_vals).argmax(dim=2)
-        return preds
+        """
+        Arguments:
+            xs {torch.Tensor} -- (source sent length, batch size, hidden size)
+            max_target_length {int} -- max target sentence length
+        
+        Returns:
+            torch.Tensor -- (max target sent length, batch size, bit size)
+        """
+        preds = self.predict_beliefs(xs, max_target_length)
+        return preds.argmax(dim=3)
 
 
 def bit_representation(num, max_size):
@@ -206,21 +225,22 @@ def bit_representation(num, max_size):
 
 
 class FraEngDataset(Dataset):
-    def __init__(self, path, spacy_model_name, gpu_id=None, num_samples=10000):
-        self.nlp = spacy.load(spacy_model_name)
-        if gpu_id is not None:
-            spacy.prefer_gpu(gpu_id)
-        self.device = get_device(gpu_id)
+    def __init__(self, path, spacy_model, device=None, num_samples=10000):
+        self.nlp = spacy_model
+        self.device = device
         self.source_docs = []
         self.target_sents = []
         self.target_bits = []
+        self.meta_data = []
         target_word_count = Counter()
         with open(path, 'r', encoding='utf-8') as f:
             lines = f.read().split('\n')
         for line in lines[: min(num_samples, len(lines) - 1)]:
             x, y, _ = line.split('\t')
+            y = y.lower()
+            self.meta_data.append({ 'source': x, 'target': y })
             self.source_docs.append(self.nlp.tokenizer(x))
-            y = y.lower().split(' ')
+            y = y.split(' ')
             for word in y:
                 target_word_count[word] += 1
             self.target_sents.append(y)
@@ -234,10 +254,10 @@ class FraEngDataset(Dataset):
             for rank, (word, _) in enumerate(target_word_count.most_common(), 1)
         }
         for sent in self.target_sents:
-            self.target_bits.append(
-                torch.tensor(
-                    [self.rank[word] for word in sent],
-                    dtype=torch.int))
+            sent_tensor = torch.tensor(
+                [self.rank[word] for word in sent],
+                dtype=torch.long)
+            self.target_bits.append(sent_tensor)
 
     def __len__(self):
         return len(self.source_docs)
@@ -249,7 +269,8 @@ class FraEngDataset(Dataset):
         
         Returns:
             torch.Tensor -- (source sent length, hidden size)  e.g., output of BERT
-            torch.Tensor -- (target sent length, self.bit_size) bit representation of target sentence
+            torch.Tensor -- (target sent length, bit size) bit representation of target sentence
+            dict -- meta data concerning the example
         """
         xs = torch.from_numpy(
                 self.source_docs[index]._.trf_last_hidden_state)
@@ -267,11 +288,12 @@ def collate_fun(batch):
     Returns:
         torch.Tensor -- (source sent length, batch size, hidden size)
         torch.Tensor -- (target sent length, batch size, bit size)
+        List[Dict[str, str]] -- list of meta data
     """
-    xs, ys = zip(*batch)
+    xs, ys, meta_data = zip(*batch)
     xs = torch.nn.utils.rnn.pad_sequence(xs)
     ys = torch.nn.utils.rnn.pad_sequence(ys)
-    return xs, ys
+    return xs, ys, meta_data
 
 
 def save_model(model, file_path):
@@ -284,42 +306,71 @@ def load_model(model, file_path):
         model.load_state_dict(torch.load(f))
             
 
+def test(model, dataset, cfg, threshold=None):
+    total_correct = total_vars = 0
+    for xs, ys, _ in DataLoader(dataset,
+            batch_size=cfg.batch_size, collate_fn=collate_fun):
+        if threshold is None:
+            preds = model.predict(xs, ys.size(0))
+        else:
+            node_beliefs = model.predict_beliefs(xs, ys.size(0))[:, :, :, 1]
+            preds = (node_beliefs > threshold).long()
+        total_correct += (preds == ys).float().sum()
+        total_vars += ys.numel()
+    acc = (total_correct / total_vars).item()
+    return acc
+
+
+def test_with_thresholds(model, dataset, cfg):
+    num_vars = 0
+    thresholds = np.arange(0.05, 0.80, 0.05)
+    total_accs = np.zeros_like(thresholds)
+    for xs, ys, _ in DataLoader(dataset,
+            batch_size=cfg.batch_size, collate_fn=collate_fun):
+        num_vars += ys.numel()
+        node_beliefs = model.predict_beliefs(xs, ys.size(0))[:, :, :, 1]
+        for i, threshold in enumerate(thresholds):
+            preds = (node_beliefs > threshold).long()
+            total_accs[i] += (preds == ys).float().sum()
+    accs = total_accs / num_vars
+    best = accs.argmax()
+    return accs[best], thresholds[best]
+
+
 def train(model, dataset, val_data, cfg, train_logger, val_logger):
-    global best_f1, best_epoch, best_threshold
-    best_f1, best_epoch, best_threshold = 0., -1, -1
+    global best_acc, best_epoch, best_threshold
+    best_acc, best_epoch, best_threshold = 0., -1, -1
 
     def validation(epoch):
-        global best_f1, best_epoch, best_threshold
+        global best_acc, best_epoch, best_threshold
         model.eval()
         if cfg.tune_thresholds:
             test_fun = test_with_thresholds
         else:
             test_fun = lambda *args: (test(*args), None)
 
-        (acc, prec, rec, f1), threshold = test_fun(model, dataset, cfg)
+        acc, threshold = test_fun(model, dataset, cfg)
         logger.info(
-            f'train results: {(acc, prec, rec, f1)}, (threshold: {threshold})')
-        train_logger.plot_for_current_epoch('F1', f1)
+            f'train results: {acc}, (threshold: {threshold})')
         train_logger.plot_for_current_epoch('Accuracy', acc)
         if val_data is not None:
-            (acc, prec, rec, f1), threshold = test_fun(model, val_data, cfg)
+            acc, threshold = test_fun(model, val_data, cfg)
             logger.info(
-                f'val results: {(acc, prec, rec, f1)}, (threshold: {threshold})')
-            val_logger.plot_for_current_epoch('F1', f1)
+                f'val results: {acc}, (threshold: {threshold})')
             val_logger.plot_for_current_epoch('Accuracy', acc)
 
-        if f1 > best_f1:
-            best_f1 = f1
+        if acc > best_acc:
+            best_acc = acc
             best_epoch = epoch
             if cfg.tune_thresholds:
                 best_threshold = threshold
-            logger.info('new best f1 found, saving model')
+            logger.info('new best accuracy found, saving model')
             save_model(model, 'best_model')
 
         save_model(model, f'model_checkpoint_{epoch}')
         subset = 'val' if val_data is not None else 'train'
         logger.info(
-            f'best {subset} results (epoch {best_epoch}, threshold {best_threshold}): {best_f1}')
+            f'best {subset} results (epoch {best_epoch}, threshold {best_threshold}): {best_acc}')
 
     train_data_loader = DataLoader(dataset,
                                    batch_size=cfg.batch_size,
@@ -334,9 +385,8 @@ def train(model, dataset, val_data, cfg, train_logger, val_logger):
         if epoch % cfg.val_interval == 0:
             validation(epoch)
         avg_loss, count = 0, 0
-        for xs, ys, onehot_ys in train_data_loader:
-            if not cfg.use_cross_ent:
-                ys = onehot_ys
+        for xs, ys, meta_data in train_data_loader:
+            ys = onehot(ys)
             model.train()
             model.zero_grad()
             loss = model.loss(xs, ys)
@@ -355,34 +405,31 @@ def train(model, dataset, val_data, cfg, train_logger, val_logger):
     validation(cfg.num_epochs)
 
 
-@hydra.main(config_path='config.yaml')
+@hydra.main(config_path='configs/spennat.yaml')
 def main(cfg: DictConfig) -> None:
     logger.info(cfg.pretty())
 
     device = get_device(cfg.device)
-    dataset = FraEngDataset(cfg.dataset, cfg.spacy_model)
+    if cfg.device >= 0:
+        spacy.prefer_gpu(cfg.device)
+    spacy_model = spacy.load(cfg.spacy_model)
+    dataset = FraEngDataset(
+            hydra.utils.to_absolute_path(cfg.dataset),
+            spacy_model,
+            device=device,
+            num_samples=cfg.num_samples)
+    hidden_size = spacy_model.get_pipe('trf_tok2vec').token_vector_width
     max_bit_size = dataset.bit_size
-    model = SPENModel(cfg.model, max_bit_size, cfg).to(device)
+    model = SPENModel(hidden_size, max_bit_size, cfg).to(device)
 
-    with TensorBoard(f'{cfg.model.name}_train') as logger:
+    with TensorBoard('spennat_train') as train_logger, \
+            TensorBoard('spennat_val') as val_logger:
         train(model,
               dataset,
+              None,
               cfg,
-              logger)
-
-    logger.info('loading best model...')
-    load_model(model, 'best_model')
-    logger.info('finding best threshold on validation data...')
-    (val_acc, val_prec ,val_rec ,val_f1), threshold = test_with_thresholds(model, val_data, cfg)
-    logger.info('testing on training data...')
-    train_acc, train_prec ,train_rec ,train_f1 = test(model, train_data, cfg, threshold=threshold)
-    logger.info('testing on test data...')
-    test_acc, test_prec ,test_rec ,test_f1 = test(model, test_data, cfg, threshold=threshold)
-
-    logger.info(f'final results: (threshold: {threshold})')
-    logger.info(f'train: {train_acc}, {train_prec}, {train_rec}, {train_f1}')
-    logger.info(f'val:   {val_acc}, {val_prec}, {val_rec}, {val_f1}')
-    logger.info(f'test:  {test_acc}, {test_prec}, {test_rec}, {test_f1}')
+              train_logger,
+              val_logger)
             
 
 if __name__ == '__main__':
